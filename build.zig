75014868e96ae26b190144e1a69bb77e595e65dd
FalsePattern <me@falsepattern.com>
2024-08-07 07:18:00 +0200

    simplify build flow

--- build.zig
+++ build.zig
@@ -16,121 +16,29 @@ pub fn build(b: *std.Build) !void {
 
     const ps2 = try Toolchain.fromEnv(b);
 
-    const ee_tool = ps2.dev.ee.tool();
+    const translate_c = ps2.createTranslateC(b, .ee, b.path("src/c.h"), optimize);
+    translate_c.addIncludeDir(ps2.extras.gsKit.include);
 
-    const ctx: Ctx = .{
-        .b = b,
-        .ps2 = ps2,
-        .artifact = exe,
-        .optimize = optimize,
-        .processor = .ee,
-    };
+    exe.root_module.addImport("c", translate_c.createModule());
 
-    _ = ctx.addCpuImport("c", b.path("src/c.h"));
-
-    var compile_obj = b.addSystemCommand(&.{ ee_tool.gcc, "-D_EE", "-nostdlib", "-D_SIGNAL_H_", "-gdwarf-2", "-gz", "-Wno-incompatible-pointer-types", "-Wno-address-of-packed-member" });
-    compile_obj.setName("[EE] Compile example.o");
-    switch (optimize) {
-        .Debug => {},
-        .ReleaseSafe => {
-            compile_obj.addArg("-O2");
-            compile_obj.addArg("-G0");
-        },
-        .ReleaseFast => {
-            compile_obj.addArg("-O2");
-            compile_obj.addArg("-G0");
-            compile_obj.addArg("-s");
-        },
-        .ReleaseSmall => {
-            compile_obj.addArg("-Os");
-            compile_obj.addArg("-G0");
-            compile_obj.addArg("-s");
-        },
-    }
-    compile_obj.addArgs(&.{ "-I", b.graph.zig_lib_directory.path.? });
+    var compile_obj = ps2.createCompileStep(b, .ee, optimize);
     compile_obj.addArg("-c");
     compile_obj.addFileArg(exe.getEmittedBin());
     compile_obj.addArg("-o");
     const compiled_obj = compile_obj.addOutputFileArg("example.o");
 
-    var compile_elf = b.addSystemCommand(&.{ ee_tool.gcc, "-flto" });
-    compile_elf.setName("[EE] Link example.elf");
-    switch (optimize) {
-        .Debug => {},
-        .ReleaseSafe, .ReleaseFast => {
-            compile_elf.addArg("-O2");
-            compile_elf.addArg("-G0");
-        },
-        .ReleaseSmall => {
-            compile_elf.addArg("-Os");
-            compile_elf.addArg("-G0");
-        },
-    }
-    compile_elf.addArgs(&.{ "-T", ps2.sdk.ee.resolve("startup/linkfile") });
+    var compile_elf = ps2.createLinkStep(b, .ee, optimize);
+    compile_elf.addFileArg(compiled_obj);
     compile_elf.addArg("-o");
     const compiled_elf = compile_elf.addOutputFileArg("example.elf");
-    compile_elf.addFileArg(compiled_obj);
-    compile_elf.addArgs(&.{ "-L", ps2.sdk.ee.resolve("lib") });
-    compile_elf.addArgs(&.{ "-L", ps2.sdk.gsKit.resolve("lib") });
-    compile_elf.addArgs(&.{ "-Wl,-zmax-page-size=128", "-ldebug", "-lc", "-lpthread", "-latomic", "-lgskit", "-ldmakit" });
+    compile_elf.addArgs(&.{ "-L", ps2.extras.gsKit.resolve(b, "lib") });
+    compile_elf.addArgs(&.{ "-ldebug", "-latomic", "-lgskit", "-ldmakit" });
 
-    _, const final_file: std.Build.LazyPath = switch (optimize) {
-        .Debug, .ReleaseSafe => .{ &compile_elf.step, compiled_elf },
-        .ReleaseSmall, .ReleaseFast => blk: {
-            const strip_elf = b.addSystemCommand(&.{ ee_tool.strip, "-s" });
-            strip_elf.setName("[EE] Strip example.elf");
-            strip_elf.addFileArg(compiled_elf);
-            strip_elf.addArg("-o");
-            const stripped_elf = strip_elf.addOutputFileArg("example.elf");
-            break :blk .{ &strip_elf.step, stripped_elf };
-        },
-    };
+    const stripped_elf = ps2.stripElf(b, .ee, optimize, compiled_elf);
 
-    const install_file = b.addInstallFileWithDir(final_file, .bin, "example.elf");
+    const install_file = b.addInstallFileWithDir(stripped_elf, .bin, "example.elf");
     b.getInstallStep().dependOn(&install_file.step);
 }
-
-const Ctx = struct {
-    b: *std.Build,
-    ps2: Toolchain,
-    artifact: *std.Build.Step.Compile,
-    optimize: std.builtin.OptimizeMode,
-    processor: Processor,
-
-    fn addCpuImportSysHeader(
-        self: Ctx,
-        name: []const u8,
-    ) *std.Build.Step.TranslateC {
-        const fname = self.b.fmt("{s}.h", .{name});
-        const import_name = self.b.fmt("<{s}>", .{fname});
-        const wf = self.b.addWriteFiles();
-        const the_file = wf.add(fname, self.b.fmt("#import {s}", .{import_name}));
-        return self.addCpuImport(
-            import_name,
-            the_file,
-        );
-    }
-
-    fn addCpuImport(
-        self: Ctx,
-        name: []const u8,
-        root_source_file: std.Build.LazyPath,
-    ) *std.Build.Step.TranslateC {
-        const translate_c = self.b.addTranslateC(.{
-            .root_source_file = root_source_file,
-            .optimize = self.optimize,
-            .target = self.processor.target(),
-        });
-        translate_c.defineCMacro(self.processor.macro(), null);
-        translate_c.addIncludeDir(self.ps2.dev.get(self.processor).tool().include.?);
-        translate_c.addIncludeDir(self.ps2.sdk.get(self.processor).include);
-        translate_c.addIncludeDir(self.ps2.sdk.common.include);
-        translate_c.addIncludeDir(self.ps2.sdk.gsKit.include);
-
-        self.artifact.root_module.addImport(name, translate_c.createModule());
-        return translate_c;
-    }
-};
 
 const Processor = enum {
     ee,
@@ -157,6 +65,35 @@ const Processor = enum {
     pub fn target(self: Processor) std.Build.ResolvedTarget {
         return switch (self) {
             .ee => ee_target.get(),
+            .iop => @panic("IOP target not yet implemented!"),
+        };
+    }
+
+    pub fn defaultCompileArgs(self: Processor, optimize: std.builtin.OptimizeMode) []const []const u8 {
+        return switch (self) {
+            .ee => blk: {
+                const base = [_][]const u8{ "-D_EE", "-gdwarf-2", "-gz", "-Wno-incompatible-pointer-types", "-Wno-address-of-packed-member" };
+                break :blk switch (optimize) {
+                    .Debug => &(base ++ [0][]const u8{}),
+                    .ReleaseSafe => &(base ++ [_][]const u8{ "-O2", "-G0" }),
+                    .ReleaseFast => &(base ++ [_][]const u8{ "-O2", "-G0", "-s" }),
+                    .ReleaseSmall => &(base ++ [_][]const u8{ "-Os", "-G0", "-s" }),
+                };
+            },
+            .iop => @panic("IOP target not yet implemented!"),
+        };
+    }
+
+    pub fn defaultLinkArgs(self: Processor, optimize: std.builtin.OptimizeMode) []const []const u8 {
+        return switch (self) {
+            .ee => blk: {
+                const base = [_][]const u8{"-Wl,-zmax-page-size=128", "-lm", "-lcdvd", "-lpthread", "-lpthreadglue", "-lcglue", "-lkernel"};
+                break :blk switch (optimize) {
+                    .Debug => &(base ++ [_][]const u8{"-lg"}),
+                    .ReleaseSafe, .ReleaseFast => &(base ++ [_][]const u8{"-O2", "-G0", "-lc"}),
+                    .ReleaseSmall => &(base ++ [_][]const u8{"-Os", "-G0", "-lc"}),
+                };
+            },
             .iop => @panic("IOP target not yet implemented!"),
         };
     }
@@ -202,33 +139,27 @@ const Processor = enum {
 };
 
 const Toolchain = struct {
-    dev: struct {
-        ee: DevDir,
-        iop: DevDir,
+    const DevSet = struct {
+        ee: Dev,
+        iop: Dev,
 
         const Self = @This();
 
-        pub fn get(self: Self, p: Processor) DevDir {
+        pub fn get(self: Self, p: Processor) Dev {
             return switch (p) {
                 .ee => self.ee,
                 .iop => self.iop,
             };
         }
-    },
-    sdk: struct {
-        ee: SdkDir,
-        iop: SdkDir,
-        common: SdkDir,
-        gsKit: SdkDir,
+    };
 
-        const Self = @This();
-        pub fn get(self: Self, p: Processor) SdkDir {
-            return switch (p) {
-                .ee => self.ee,
-                .iop => self.iop,
-            };
-        }
-    },
+    const ExtraSdks = struct {
+        common: Sdk,
+        gsKit: Sdk,
+    };
+
+    extras: ExtraSdks,
+    dev: DevSet,
 
     pub fn fromEnv(b: *std.Build) !Toolchain {
         const dev_root = std.posix.getenv("PS2DEV") orelse {
@@ -239,81 +170,120 @@ const Toolchain = struct {
             std.log.err("PS2SDK environment variable missing!", .{});
             return error.EnvMissing;
         };
+        const sdk_ee = Sdk.from(b, sdk_root, Processor.ee.suffix());
+        const sdk_iop = Sdk.from(b, sdk_root, Processor.iop.suffix());
+        const extras = ExtraSdks{
+            .common = Sdk.from(b, sdk_root, "common"),
+            .gsKit = Sdk.from(b, dev_root, "gsKit"),
+        };
+        const dev = DevSet{
+            .ee = Dev.from(b, dev_root, .ee, sdk_ee),
+            .iop = Dev.from(b, dev_root, .iop, sdk_iop),
+        };
         return .{
-            .dev = .{
-                .ee = DevDir.from(b, dev_root, .ee),
-                .iop = DevDir.from(b, dev_root, .iop),
-            },
-            .sdk = .{
-                .ee = SdkDir.from(b, sdk_root, Processor.ee.suffix()),
-                .iop = SdkDir.from(b, sdk_root, Processor.iop.suffix()),
-                .common = SdkDir.from(b, sdk_root, "common"),
-                .gsKit = SdkDir.from(b, dev_root, "gsKit"),
+            .extras = extras,
+            .dev = dev,
+        };
+    }
+
+    pub fn createTranslateC(self: Toolchain, b: *std.Build, processor: Processor, root_source_file: std.Build.LazyPath, optimize: std.builtin.OptimizeMode) *std.Build.Step.TranslateC {
+        const dev = self.dev.get(processor);
+        const translate_c = b.addTranslateC(.{
+            .root_source_file = root_source_file,
+            .optimize = optimize,
+            .target = processor.target(),
+        });
+        translate_c.defineCMacro(processor.macro(), null);
+        translate_c.addIncludeDir(dev.stdlibIncludePath(b).?);
+        translate_c.addIncludeDir(dev.sdk.include);
+        translate_c.addIncludeDir(self.extras.common.include);
+        return translate_c;
+    }
+
+    pub fn createCompileStep(self: Toolchain, b: *std.Build, processor: Processor, optimize: std.builtin.OptimizeMode) *std.Build.Step.Run {
+        const dev = self.dev.get(processor);
+        var cmd = b.addSystemCommand(&.{
+            dev.tool(b, "gcc"),
+        });
+        cmd.addArgs(processor.defaultCompileArgs(optimize));
+        cmd.addArgs(&.{"-I", b.graph.zig_lib_directory.path.?});
+        return cmd;
+    }
+
+    pub fn createLinkStep(self: Toolchain, b: *std.Build, processor: Processor, optimize: std.builtin.OptimizeMode) *std.Build.Step.Run {
+        const dev = self.dev.get(processor);
+        var cmd = b.addSystemCommand(&.{
+            dev.tool(b, "gcc"),
+            "-T",
+            dev.sdk.resolve(b, "startup/linkfile"),
+            "-L",
+            dev.sdk.resolve(b, "lib"),
+        });
+        cmd.addArgs(processor.defaultLinkArgs(optimize));
+        return cmd;
+    }
+
+    pub fn stripElf(self: Toolchain, b: *std.Build, processor: Processor, optimize: std.builtin.OptimizeMode, file: std.Build.LazyPath) std.Build.LazyPath {
+        const dev = self.dev.get(processor);
+        return switch (optimize) {
+            .Debug, .ReleaseSafe => file,
+            .ReleaseSmall, .ReleaseFast => blk: {
+                const strip_elf = b.addSystemCommand(&.{ dev.tool(b, "strip"), "-s" });
+                strip_elf.addFileArg(file);
+                strip_elf.addArg("-o");
+                const stripped_elf = strip_elf.addOutputFileArg("stripped.elf");
+                break :blk stripped_elf;
             },
         };
     }
 };
 
-const Tool = struct {
-    b: *std.Build,
-    gcc: []const u8,
-    strip: []const u8,
-    include: ?[]const u8,
-};
-
-const DevDir = struct {
-    b: *std.Build,
+const Dev = struct {
+    processor: Processor,
+    sdk: Sdk,
     root: []const u8,
-    tool_prefix: []const u8,
 
-    pub fn from(b: *std.Build, base: []const u8, p: Processor) DevDir {
+    pub fn from(b: *std.Build, base: []const u8, p: Processor, sdk: Sdk) Dev {
         return .{
-            .b = b,
+            .processor = p,
+            .sdk = sdk,
             .root = b.fmt("{s}/{s}", .{ base, p.suffix() }),
-            .tool_prefix = p.toolPrefix(),
         };
     }
+    
+    pub fn tool(self: Dev, b: *std.Build, tool_name: []const u8) []u8 {
+        return b.pathJoin(&.{self.root, "bin", b.fmt("{s}-{s}", .{self.processor.toolPrefix(), tool_name},)});
+    }
 
-    pub fn tool(self: DevDir) Tool {
-        const include_path = self.resolve(self.b.fmt("{s}/{s}", .{ self.tool_prefix, "include" }));
-
-        const exists: bool = if (std.fs.openDirAbsolute(include_path, .{})) |dir| blk: {
+    pub fn stdlibIncludePath(self: Dev, b: *std.Build) ?[]const u8 {
+        const include_path = self.resolve(b, b.fmt("{s}/{s}", .{ self.processor.toolPrefix(), "include" }));
+        if (std.fs.openDirAbsolute(include_path, .{})) |dir| {
             var d = dir;
             d.close();
-            break :blk true;
-        } else |_| false;
-        return .{
-            .b = self.b,
-            .gcc = self.b.fmt("{s}/bin/{s}-gcc", .{ self.root, self.tool_prefix }),
-            .strip = self.b.fmt("{s}/bin/{s}-strip", .{ self.root, self.tool_prefix }),
-            .include = if (exists) include_path else null,
-        };
+            return include_path;
+        } else |_| {
+            return null;
+        }
     }
 
-    pub fn resolve(self: DevDir, rel_path: []const u8) []u8 {
-        return self.b.fmt("{s}/{s}", .{ self.root, rel_path });
-    }
-
-    pub fn resolveSubTool(self: DevDir, rel_path: []const u8) []u8 {
-        return self.b.fmt("{s}/{s}/{s}", .{ self.root, self.tool_prefix, rel_path });
+    pub fn resolve(self: Dev, b: *std.Build, rel_path: []const u8) []u8 {
+        return b.fmt("{s}/{s}", .{ self.root, rel_path });
     }
 };
 
-const SdkDir = struct {
-    b: *std.Build,
+const Sdk = struct {
     root: []const u8,
     include: []const u8,
 
-    pub fn from(b: *std.Build, base: []const u8, suffix: []const u8) SdkDir {
+    pub fn from(b: *std.Build, base: []const u8, suffix: []const u8) Sdk {
         const root = b.fmt("{s}/{s}", .{ base, suffix });
         return .{
-            .b = b,
             .root = root,
             .include = b.fmt("{s}/include", .{root}),
         };
     }
 
-    pub fn resolve(self: SdkDir, rel_path: []const u8) []u8 {
-        return self.b.fmt("{s}/{s}", .{ self.root, rel_path });
+    pub fn resolve(self: Sdk, b: *std.Build, rel_path: []const u8) []u8 {
+        return b.fmt("{s}/{s}", .{ self.root, rel_path });
     }
 };
